05b22bda046de6bebd6aad56294aa99bc29422dc
Rittidej Wongpiya <>
2022-02-17 14:16:08 +0700

    Resolve footer

--- public/components/Footer.js
+++ public/components/Footer.js
@@ -185,8 +185,8 @@ class Footer extends HTMLElement {
           </div>
           <div class="tel-mobile"><a href="tel:+66 2538 8019">+66 2538 8019</a></div>
           <ul class="footer-socials ps-0">
-            <a href="https://www.facebook.com/tenthai.info" target="_blank"><img src="./assets/home/facebook-pink.svg" alt="facebook" /></a>
-            <a href="https://www.instagram.com/explore/tags/tenthai/?hl=en" target="_blank"><img src="./assets/home/instagram-pink.svg" alt="instagram" /></a>
+            <a href="https://www.facebook.com/tenthai.info" target="_blank"><i class="fab fa-facebook-f"></i></a>
+            <a href="https://www.instagram.com/explore/tags/tenthai/?hl=en" target="_blank"><i class="fab fa-instagram"></i></a>
             <a href="https://lin.ee/hlS0zzw" target="_blank"><img src="./assets/home/line-pink.svg" alt="line" /></a>
           </ul>
         </div>
